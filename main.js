488453427cd018c6c807e42005b7c7b7507a1b25
Trenton Daniels <trentondaniels95@gmail.com>
2018-05-03 18:50:41 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+const about = document.getElementById("#about");
+const product = document.getElementById("#product");
+const pricing = document.getElementById("#pricing");
+const shop = document.getElementById("#shop");
+
+isActive = function () {
+	this.className.add('isActive');
+}
